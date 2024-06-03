16a2eadfe3237ef58b27ec0b60e22d6b32e373df
gamename <tennis.n.smith@gmail.com>
2024-06-03 13:16:24 -0500

    set to RequestBody type

--- app/src/main/java/com/example/cdbv4_pixel_app/services/ApiService.kt
+++ app/src/main/java/com/example/cdbv4_pixel_app/services/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.cdbv4_pixel_app.services
 
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
@@ -7,5 +8,5 @@ import retrofit2.http.POST
 
 interface ApiService {
     @POST(Constants.BASE_URL)
-    suspend fun sendAlert(@Body requestBody: com.example.cdbv4_pixel_app.services.RequestBody): Response<ResponseBody>
+    suspend fun sendAlert(@Body requestBody: RequestBody): Response<ResponseBody>
 }
