10f2f4b203badb8bb8d6ad7c22161a22d8a89302
gamename <tennis.n.smith@gmail.com>
2024-06-06 04:22:07 -0500

    cat detected and SMS generated

---
+++ app/src/main/java/com/example/cdbv4_pixel_app/services/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.cdbv4_pixel_app.services
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("ring")
+    suspend fun sendAlert(@Body requestBody: CatAlertRequestBody): Response<Any>
+}
