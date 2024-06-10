80420b433120456a5ae4191451371fa46c6e0db9
gamename <tennis.n.smith@gmail.com>
2024-06-10 14:11:09 -0500

    add logic to start app at boot time

---
+++ app/src/main/java/com/example/cdbv4_pixel_app/BootReceiver.kt
@@ -0,0 +1,15 @@
+package com.example.cdbv4_pixel_app
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            val activityIntent = Intent(context, MainActivity::class.java)
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(activityIntent)
+        }
+    }
+}
