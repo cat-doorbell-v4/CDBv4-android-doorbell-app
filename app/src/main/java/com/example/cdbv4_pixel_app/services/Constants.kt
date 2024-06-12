25599b865071c5f5796e63d58e5f15666accaacb
gamename <tennis.n.smith@gmail.com>
2024-06-11 19:36:34 -0500

    add low-light threshold

--- app/src/main/java/com/example/cdbv4_pixel_app/services/Constants.kt
+++ app/src/main/java/com/example/cdbv4_pixel_app/services/Constants.kt
@@ -2,4 +2,6 @@ package com.example.cdbv4_pixel_app.services
 
 object Constants {
     const val BASE_URL = "https://eq1qnupb76.execute-api.us-east-1.amazonaws.com/prod1/"
+    const val LOW_LIGHT_THRESHOLD = 5
+
 }
